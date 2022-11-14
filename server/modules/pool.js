dc3cc6430f894ad8fa2dbd0261b8c06720245075
Andrew Carey <andrew.r.carey@gmail.com>
2022-11-14 15:47:43 -0600

    updated pg for bit.io hosting

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,28 @@
 const pg = require('pg');
-const Pool = pg.Pool;
-const pool = new Pool({
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimoutMillis: 30000
+let pool;
+
+if (process.env.DATABASE_URL){
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
 });
+}
+else {
+;
+    pool = new pg.Pool({
+        database: 'weekend-to-do-app',
+        host: 'localhost',
+        port: 5432,
+        max: 10,
+        idleTimoutMillis: 30000
+    });
+}
+
+
+
+
 
 pool.on('connect', () => {
     console.log('Connected to database');
